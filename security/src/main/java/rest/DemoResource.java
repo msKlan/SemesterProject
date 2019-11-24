900aca39a2aa652bd81f3abafc24cc2ba4d2f9d1
Klan <michaelsanderklan@gmail.com>
2019-11-24 19:15:11 +0100

    updated look up places and structural changes

--- security/src/main/java/rest/DemoResource.java
+++ security/src/main/java/rest/DemoResource.java
@@ -86,7 +86,6 @@ public class DemoResource {
     }
     
     @GET
-<<<<<<< HEAD
     @Path("flights/{origin}/{destination}/{date}")
     @Produces(MediaType.APPLICATION_JSON)
     public String getJsonFlight(@PathParam("origin") String origin, @PathParam("destination") String destination, @PathParam("date") String date) throws ProtocolException, IOException{
@@ -99,11 +98,4 @@ public class DemoResource {
     public String getSessionID() throws ProtocolException, IOException{
         return api.getCustomerSessionID();
     }
-=======
-    @Path("{origin}/{destination}/{date}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getJsonFlight(@PathParam("origin") String origin, @PathParam("destination") String destination, @PathParam("date") Date date) throws ProtocolException, IOException{
-        return api.getFlightData(origin, destination, date);
-    }
->>>>>>> 916f62955bc3c1b07643001a35a18a13f5ed8de8
 }
